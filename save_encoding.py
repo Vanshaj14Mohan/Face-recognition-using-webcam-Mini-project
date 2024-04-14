5eff4481386eeb7d4afb156ea307a06056eb1297
Vanshaj P Mohan <Vanshajkumar145@gmail.com>
2024-04-14 15:53:18 +0530

    project

--- save_encoding.py
+++ save_encoding.py
@@ -1,3 +1,4 @@
+#Importing libraries
 import face_recognition
 import os
 import pickle
@@ -12,6 +13,16 @@ for image in images:
         'enc': face_recognition.face_encodings(ki)[0]
     })
 
+#Accessing know images:
+known_image = face_recognition.load_image_file("bill gates.jpg")
+known_image = face_recognition.load_image_file("vanshaj.jpg")
+known_image = face_recognition.load_image_file("zaid kamil.jpg")
+
+billgates_encoding = face_recognition.face_encodings(known_image)[0]
+vanshaj_encoding = face_recognition.face_encodings(known_image)[0]
+zaid_kamil_encoding = face_recognition.face_encodings(known_image)[0]
+
+#saving encoding to files
 for i in known_images_enc:
     with open(f"encoding/{i['name']}.enc", 'wb') as f:
         pickle.dump(i['enc'], f)
--- webcam.py
+++ webcam.py
@@ -3,23 +3,70 @@ import os
 import numpy as np
 import cv2
 import face_recognition
-face_detector1=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-#Reading the input image now. 
-cam = cv2.VideoCapture(0) #index value of webcam
-while cam.isOpened():
-    state, frame = cam.read() # state tells about image from webcam
-    if not state:
-        print("no frame")
-        break
-    blue = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  
-    # img =cv2.imread("vanshaj.jpg")
-    faces= face_detector1.detectMultiScale(blue, 1.3, 5)
-    for (x,y, w, h) in faces:
-        cv2.rectangle(frame, pt1 = (x,y),pt2 = (x+w, y+h), color = (0,255,0),thickness =  3)
-        roi_blue = blue[y:y+h,x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
-    cv2.imshow("cam1", frame)
-    if cv2.waitKey(1) == 27: #27 is for exit button. or == ord("q")
-        break
-cam.release()
-cv2.destroyAllWindows()
+
+
+def recognize():
+    encodings = os.listdir("encoding")
+    images = os.listdir("images")
+    # load data
+    known_face_encodings = []
+    known_face_names = []
+    for path, enc in zip(images, encodings):
+        img= face_recognition.load_image_file(f'images/{path}')
+        known_face_encodings.append(face_recognition.face_encodings(img)[0])
+        known_face_names.append(path.split(".")[0])
+    #Reading the input image now. 
+    face_locations = []
+    face_encodings = []
+    face_names = []
+    process_this_frame = True
+
+    cam = cv2.VideoCapture(0) #index value of webcam
+    face_detector1=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    while True:
+        ret, frame = cam.read()
+        if process_this_frame:
+            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+            rgb_small_frame = small_frame[:, :, ::-1]
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+            face_names = []
+            for face_encoding in face_encodings:
+                matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+                name = "Unknown"
+                
+            # Or instead, use the known face with the smallest distance to the new face
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
+
+            face_names.append(name)
+
+    process_this_frame = not process_this_frame
+
+     # Displaying the results
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        # Drawing a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 255, 0), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (0, 255, 0), 1)
+
+        cv2.imshow(frame)
+        if cv2.waitKey() == 27: 
+            break
+    cam.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    recognize()
