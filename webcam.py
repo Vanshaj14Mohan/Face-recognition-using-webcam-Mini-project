32951409c89004830df05e584ab04f0250733cd9
Vanshaj P Mohan <Vanshajkumar145@gmail.com>
2024-04-14 16:11:01 +0530

    project

--- webcam.py
+++ webcam.py
@@ -1,4 +1,3 @@
-import requests
 import os
 import numpy as np
 import cv2
@@ -43,30 +42,29 @@ def recognize():
 
             face_names.append(name)
 
-    process_this_frame = not process_this_frame
+        process_this_frame = not process_this_frame
 
-     # Displaying the results
-    for (top, right, bottom, left), name in zip(face_locations, face_names):
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
+        # Displaying the results
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
 
-        # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-        # Drawing a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 255, 0), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (0, 255, 0), 1)
+            # Drawing a label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 255, 0), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (0, 255, 0), 1)
 
-        cv2.imshow(frame)
-        if cv2.waitKey() == 27: 
-            break
-    cam.release()
-    cv2.destroyAllWindows()
-
+            cv2.imshow(frame)
+            if cv2.waitKey() == 27: 
+                break
+        cam.release()
+        cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     recognize()
